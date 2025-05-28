09b3e921fffda27bfe25fcbcd76fe8e928db52a9
Giovani <giovani.guedes@outlook.com>
2025-05-28 06:06:03 -0300

    Update churchPanel.js

--- churchPanel.js
+++ churchPanel.js
@@ -27,7 +27,7 @@ const customStyle = `
     top: 20px;
     right: 20px;
     z-index: 9999;
-    width: 300px;
+    width: 280px;
     background: #2e3440;
     padding: 20px;
     border-radius: 10px;
