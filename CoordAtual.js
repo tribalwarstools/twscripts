0e3d0d0e5ae8c6ca65b8d0fbdfa4786e4d996b9b
Giovani <giovani.guedes@outlook.com>
2025-06-02 23:53:28 -0300

    Update CoordAtual.js

--- CoordAtual.js
+++ CoordAtual.js
@@ -115,7 +115,7 @@
             const remover = document.createElement('button');
             remover.className = 'btn btn-cancel';
             remover.style.padding = '2px 6px';
-            remover.innerText = '';
+            //remover.innerText = 'Remover';
             remover.title = 'Remover aldeia dos favoritos';
             remover.addEventListener('click', () => {
                 window.removeFavorito(index);
