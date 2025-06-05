22b069b2b3758bf068d02f1a2a1da0c7e728c20c
Giovani <giovani.guedes@outlook.com>
2025-06-05 06:59:19 -0300

    Update village-selector.js

--- village-selector.js
+++ village-selector.js
@@ -32,8 +32,8 @@
   const html = `
     <div class="vis" style="padding: 10px;">
       <h2>Painel de Scripts</h2>
-      <button id="abrirRenamer" class="btn btn-confirm-yes" style="margin-bottom:10px;">Renomeador</button>
-      <button id="abrirTotalTropas" class="btn btn-confirm-yes" style="margin-bottom:10px;">Total de Tropas</button>
+      <button id="abrirRenamer" class="btn btn-confirm-yes" style="margin-bottom:10px;">Renomear aldeias</button>
+      <button id="abrirTotalTropas" class="btn btn-confirm-yes" style="margin-bottom:10px;">Contador de tropas</button>
       <div style="display: flex; align-items: center; gap: 10px;">
         <label for="groupSelect"><b>Visualizador de grupo:</b></label>
         <select id="groupSelect" style="padding:4px; background:#f4e4bc; color:#000; border:1px solid #603000; font-weight:bold;"></select>
