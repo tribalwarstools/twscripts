f9735c5c6d917d1a4e89d6aea4ec8aec2619f821
Giovani <giovani.guedes@outlook.com>
2025-08-10 05:03:56 -0300

    Update antiLogoffRobusto.js

--- antiLogoffRobusto.js
+++ antiLogoffRobusto.js
@@ -7,7 +7,7 @@
 style.textContent = `
     #twPainelAntiLogoff {
       position: fixed;
-      bottom: auto;
+      bottom: 0px;
       left: 0px;
       background: #2e2e2e;
       border: 2px solid #b79755;
@@ -202,6 +202,7 @@ style.textContent = `
   window.iniciarAntiLogoffRobusto = iniciarAntiLogoffRobusto;
   window.desativarAntiLogoff = desativarAntiLogoff;
 })();
+
 
 
 
