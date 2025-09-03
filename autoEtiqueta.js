83014a0480f11eab3f248cff3ffbe9cb7b5e9360
Giovani <giovani.guedes@outlook.com>
2025-09-03 03:06:44 -0300

    Update autoEtiqueta.js

--- autoEtiqueta.js
+++ autoEtiqueta.js
@@ -11,7 +11,7 @@
     #twLBL-painel { 
       position: fixed; top: 180px; left: 0; background: #2b2b2b; border: 2px solid #654321; border-left: none; 
       border-radius: 0 10px 10px 0; box-shadow: 2px 2px 8px #000; font-family: Verdana, sans-serif; color: #f1e1c1; 
-      z-index: 9997; transition: transform 0.3s ease-in-out; transform: translateX(-220px); 
+      z-index: 9997; transition: transform 0.3s ease-in-out; transform: translateX(-200px); 
     }
     #twLBL-toggle { 
       position: absolute; top: 0; right: -28px; width: 28px; height: 40px; background: #5c4023; 
