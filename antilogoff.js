59bdfbfe88f0bc94e0dcb3ed57d1194582a59048
Giovani <giovani.guedes@outlook.com>
2025-09-03 02:34:35 -0300

    Update antilogoff.js

--- antilogoff.js
+++ antilogoff.js
@@ -11,7 +11,7 @@
   const style = document.createElement('style');
   style.textContent = `
     #twAL-painel { 
-      position: fixed; top: 120px; left: 0; background: #2b2b2b; border: 2px solid #654321; border-left: none; 
+      position: fixed; top: 140px; left: 0; background: #2b2b2b; border: 2px solid #654321; border-left: none; 
       border-radius: 0 10px 10px 0; box-shadow: 2px 2px 8px #000; font-family: Verdana, sans-serif; color: #f1e1c1; 
       z-index: 9999; transition: transform 0.3s ease-in-out; transform: translateX(-180px); 
     }
@@ -174,3 +174,4 @@
   window.desativarAntiLogoff = desativarAntiLogoff;
 
 })();
+
