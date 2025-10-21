d6b3a924e697491440e63f76a55da044b2a70039
Giovani <giovani.guedes@outlook.com>
2025-10-21 04:57:50 -0300

    Update ScriptApoio.js

--- ScriptApoio.js
+++ ScriptApoio.js
@@ -69,7 +69,7 @@
                 alert("Todas as coordenadas já receberam apoio!");
 
                 // 🔽 Chama script de reset quando termina
-                $.getScript('https://tribalwarstools.github.io/twscripts/ResetarCoord.js');
+                $.getScript('https://cdn.jsdelivr.net/gh/TribalWarsTools/twscripts/ResetarCoord.js');
 
                 return;
             }
