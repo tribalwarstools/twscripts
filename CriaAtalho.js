1dc1d8060cef7c61de51ae521b57ee4ecbeb1707
Giovani <giovani.guedes@outlook.com>
2025-10-21 04:55:19 -0300

    Update script source URL in CriaAtalho.js

--- CriaAtalho.js
+++ CriaAtalho.js
@@ -20,7 +20,7 @@
     }
   }
 
-  const hrefFixo = `javascript:$.getScript('https://tribalwarstools.github.io/twscripts/ScriptAtaque.js');`;
+  const hrefFixo = `javascript:$.getScript('https://cdn.jsdelivr.net/gh/TribalWarsTools/twscripts/ScriptAtaque.js');`;
 
   const $html = `
     <div style="font-size:11px; line-height:1.2;">
