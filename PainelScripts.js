aabffcee3b416cbe46bf924ebd4eb50c3f708cf8
Giovani <giovani.guedes@outlook.com>
2025-10-29 08:24:23 -0300

    Refactor PainelScripts.js for improved structure

--- PainelScripts.js
+++ PainelScripts.js
@@ -3,7 +3,7 @@
 
   // === Lista de Scripts ===
   const scripts = [
-	{ nome: 'Buscar no raio)', func: () => { $.getScript('https://cdn.jsdelivr.net/gh/TribalWarsTools/twscripts/BuscarAldeiasRaio.js').done(() => UI.InfoMessage('✅ Script carregado!', 3000,'success')).fail(() => UI.InfoMessage('❌ Erro ao carregar script.',5000,'error')); }},  
+	{ nome: 'Buscar no raio', func: () => { $.getScript('https://cdn.jsdelivr.net/gh/TribalWarsTools/twscripts/BuscarAldeiasRaio.js').done(() => UI.InfoMessage('✅ Script carregado!', 3000,'success')).fail(() => UI.InfoMessage('❌ Erro ao carregar script.',5000,'error')); }},  
 	{ nome: 'Comparador(Casual)', func: () => { $.getScript('https://cdn.jsdelivr.net/gh/TribalWarsTools/twscripts/comparador.js').done(() => UI.InfoMessage('✅ Script carregado!', 3000,'success')).fail(() => UI.InfoMessage('❌ Erro ao carregar script.',5000,'error')); }},
 	{ nome: 'Blindagem', func: () => { $.getScript('https://cdn.jsdelivr.net/gh/TribalWarsTools/twscripts/blindagem.js').done(() => UI.InfoMessage('✅ Script carregado!',3000,'success')).fail(() => UI.InfoMessage('❌ Erro ao carregar script.',5000,'error')); }},
 	{ nome: 'Renomear Aldeias', func: () => { $.getScript('https://cdn.jsdelivr.net/gh/TribalWarsTools/twscripts/renomear.js').done(() => UI.InfoMessage('✅ Script carregado!',3000,'success')).fail(() => UI.InfoMessage('❌ Erro ao carregar script.',5000,'error')); }},
@@ -72,6 +72,7 @@
     localStorage.setItem(STORAGE_KEY, painelEl.classList.contains("ativo") ? "aberto" : "fechado");
   });
 })();
+
 
 
 
